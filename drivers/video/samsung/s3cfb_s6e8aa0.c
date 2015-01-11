b23c988a0637b96ce38f590b856761c4470739b9
HboHD <slaskiezakladyazotowe@gmail.com>
2015-01-11 12:43:38 +0100

    Revert "Allow manual brightness up to 300 candela"
    This reverts commit 2a944bdc36dbac8ee240942e29db9bc6278d2f8d.

--- drivers/video/samsung/s3cfb_s6e8aa0.c
+++ drivers/video/samsung/s3cfb_s6e8aa0.c
@@ -894,8 +894,8 @@ static int update_brightness(struct lcd_info *lcd, u8 force)
 
 	brightness = lcd->bd->props.brightness;
 
-	if (unlikely(!lcd->auto_brightness && brightness > 255))
-		brightness = 255;
+	if (unlikely(!lcd->auto_brightness && brightness > 250))
+		brightness = 250;
 
 	lcd->bl = get_backlight_level_from_brightness(brightness);
 
